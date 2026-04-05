aa748b19ca356e406cb33d6122b2d83ebc87d580
Leonardo Padilha <leonardopadilha28@gmail.com>
2026-04-05 16:29:09 -0300

    Ajustando o nome da pasta

--- playwright.config.ts
+++ playwright.config.ts
@@ -12,7 +12,7 @@ import { defineConfig, devices } from '@playwright/test';
  * See https://playwright.dev/docs/test-configuration.
  */
 export default defineConfig({
-  testDir: './playwright/e2e/actual',
+  testDir: './playwright/e2e/atual',
   /* Run tests in files in parallel */
   fullyParallel: true,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
